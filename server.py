1e58e97a0bbf4e34b1c834f2f95d9a36b8d14c8b
arjun1321 <guptaarjun152@gmail.com>
2019-02-03 19:03:10 +0530

    testing 1

--- server.py
+++ server.py
@@ -1,46 +1,25 @@
 import cherrypy
 import os
-import json
+from jinja2 import Environment, FileSystemLoader
+env = Environment(loader=FileSystemLoader('html'))
 
-
-
-
-
-
-class StockList(object):
+class HelloWorld(object):
     @cherrypy.expose
     def index(self):
-        return open("index.html")
+        data_to_show = ['Hello', 'world']
+        tmpl = env.get_template('index.html')
+        return tmpl.render(data=data_to_show)
 
-
-
-if __name__ == "__main__":
-
-    conf = {
-        'global': {
-            'server.socket_host': '0.0.0.0',
-            'server.socket_port': int(os.environ.get('PORT', 5000))
-        },
-        '/': {
-            'tools.sessions.on': True,
-            'tools.staticdir.root': os.path.abspath(os.getcwd())
-        },
-        '/stocks': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-            'tools.response_headers.on': True,
-            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
-        },
-        '/stocks/search': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-            'tools.response_headers.on': True,
-            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
-        },
-        '/static': {
-            'tools.staticdir.on': True,
-            'tools.staticdir.dir': 'public'
-        }
+config = {
+    'global': {
+        'server.socket_host': '0.0.0.0',
+        'server.socket_port': int(os.environ.get('PORT', 5000)),
+    },
+    '/assets': {
+        'tools.staticdir.root': os.path.dirname(os.path.abspath(__file__)),
+        'tools.staticdir.on': True,
+        'tools.staticdir.dir': 'assets',
     }
-    webapp = StockList()
-    # webapp.stocks = StockListService()
-    # webapp.stocks.search = StockSearchService()
-    cherrypy.quickstart(webapp, '/', conf)
+}
+
+cherrypy.quickstart(HelloWorld(), '/', config=config)
