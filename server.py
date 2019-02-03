8a324f90797f7fe7bd79dfe2f86fcdf403d7aadd
arjun1321 <guptaarjun152@gmail.com>
2019-02-03 19:17:56 +0530

    testing 4

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ config = {
         'server.socket_host': '0.0.0.0',
         'server.socket_port': int(os.environ.get('PORT', 5000)),
     },
-    '/assets': {
+    '/css': {
         'tools.staticdir.root': os.path.dirname(os.path.abspath(__file__)),
         'tools.staticdir.on': True,
         'tools.staticdir.dir': 'assets',
