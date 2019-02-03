6ee8ed11cf8a4a1eb26513ef9a0911feb2d81246
arjun1321 <guptaarjun152@gmail.com>
2019-02-03 19:22:34 +0530

    testing

--- server.py
+++ server.py
@@ -1,25 +1,46 @@
 import cherrypy
 import os
-from jinja2 import Environment, FileSystemLoader
-env = Environment(loader=FileSystemLoader('html'))
+import json
 
-class HelloWorld(object):
+
+
+
+
+
+class StockList(object):
     @cherrypy.expose
     def index(self):
-        data_to_show = ['Hello', 'world']
-        tmpl = env.get_template('index.html')
-        return tmpl.render(data=data_to_show)
+        return open("index.html")
 
-config = {
-    'global': {
-        'server.socket_host': '0.0.0.0',
-        'server.socket_port': int(os.environ.get('PORT', 5000)),
-    },
-    '/css': {
-        'tools.staticdir.root': os.path.dirname(os.path.abspath(__file__)),
-        'tools.staticdir.on': True,
-        'tools.staticdir.dir': 'assets',
+
+
+if __name__ == "__main__":
+
+    conf = {
+        'global': {
+            'server.socket_host': '0.0.0.0',
+            'server.socket_port': int(os.environ.get('PORT', 5000))
+        },
+        '/': {
+            'tools.sessions.on': True,
+            'tools.staticdir.root': os.path.abspath(os.getcwd())
+        },
+        '/stocks': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.response_headers.on': True,
+            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+        },
+        '/stocks/search': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.response_headers.on': True,
+            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+        },
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': 'public'
+        }
     }
-}
-
-cherrypy.quickstart(HelloWorld(), '/', config=config)
+    webapp = StockList()
+    # webapp.stocks = StockListService()
+    # webapp.stocks.search = StockSearchService()
+    cherrypy.quickstart(webapp, '/', conf)
