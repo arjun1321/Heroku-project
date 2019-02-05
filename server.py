aa0933b9eaf5845ed97dee387623eea5df35d029
arjun1321 <guptaarjun152@gmail.com>
2019-02-05 23:28:06 +0530

    testing connection

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     conf = {
         'global': {
-            'server.socket_host': '0.0.0.0',
+            'server.socket_host': '0.0.0.',
             'server.socket_port': int(os.environ.get('PORT', 5000))
         },
         '/': {
