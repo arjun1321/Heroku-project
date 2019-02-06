e3f8ae75dd21dc737125fd57782ea2bce296954b
arjun1321 <guptaarjun152@gmail.com>
2019-02-06 23:41:33 +0530

    testing

--- celery.py
+++ celery.py
@@ -1,10 +1,11 @@
 from celery import Celery
 from celery.schedules import crontab
+import os
 
 
-redis_url = "redis://redis:6379/0"
+redis_url = os.environ.get("REDIS_URL", 'None')
 
-app = Celery("app", broker=redis_url, backend=redis_url, include=["app.tasks"])
+app = Celery("app", broker=redis_url, backend=redis_url, include=["task"])
 
 #  app.autodiscover_tasks()
 app.conf.timezone = "Asia/Kolkata"
