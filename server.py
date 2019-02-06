52cbd8cd1d1dfedada89bab18142cf6faa18c1e4
arjun1321 <guptaarjun152@gmail.com>
2019-02-06 22:04:52 +0530

    corrected port config

--- server.py
+++ server.py
@@ -31,8 +31,8 @@ if __name__ == "__main__":
 
     conf = {
         'global': {
-            'server.socket_host': '127.0.0.1',
-            'server.socket_port': int(os.environ.get('PORT', 8090))
+            'server.socket_host': '0.0.0.0',
+            'server.socket_port': int(os.environ.get('PORT', 5000))
         },
         '/': {
             'tools.sessions.on': True,
